981a579beb747c2dd6fed47ec6c2cc48119e7f44
suzukey <suzukei0208@gmail.com>
2021-03-29 02:29:59 +0900

    ✅ Update tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,87 +1,92 @@
 import WindiSorter from '@/index'
 
 describe('Simple Sort', () => {
-  const windiSorter = new WindiSorter({})
+  it('Without group syntax', () => {
+    const windiSorter = new WindiSorter({ useVariantGroup: false })
 
-  it('Sort TailwindCSS syntax', () => {
     const classes =
       'z-50 z-10 container text-left md:text-center justify-center'
-    const correctClasses =
+    const correct =
       'container justify-center text-left z-10 z-50 md:text-center'
 
     const sortedClassList = windiSorter.sortClassNames(classes)
 
-    expect(sortedClassList).toBe(correctClasses)
+    expect(sortedClassList).toBe(correct)
   })
 
-  it('Sort WindiCSS syntax', () => {
+  it('With group syntax', () => {
+    const windiSorter = new WindiSorter({ useVariantGroup: true })
+
     const classes = 'bg-white sm:hover:(font-medium bg-gray-100) font-light'
-    const correctClasses =
-      'bg-white font-light sm:hover:(bg-gray-100 font-medium)'
+    const correct = 'bg-white font-light sm:hover:(bg-gray-100 font-medium)'
 
     const sortedClassList = windiSorter.sortClassNames(classes)
 
-    expect(sortedClassList).toBe(correctClasses)
+    expect(sortedClassList).toBe(correct)
   })
 })
 
 describe('Unknown Sort', () => {
-  const windiSorter = new WindiSorter()
+  it('Without group syntax', () => {
+    const windiSorter = new WindiSorter({ useVariantGroup: false })
 
-  it('Sort TailwindCSS syntax', () => {
     const classes =
-      'z-50 z-10 container text-left md:text-center justify-center test md:test'
-    const correctClasses =
+      'z-50 test z-10 container text-left md:text-center md:test justify-center'
+    const correct =
       'container justify-center text-left z-10 z-50 md:text-center test md:test'
 
     const sortedClassList = windiSorter.sortClassNames(classes)
 
-    expect(sortedClassList).toBe(correctClasses)
+    expect(sortedClassList).toBe(correct)
   })
 
-  it('Sort WindiCSS syntax', () => {
+  it('With group syntax', () => {
+    const windiSorter = new WindiSorter({ useVariantGroup: true })
+
     const classes =
       'bg-white sm:hover:(font-medium bg-gray-100 test) font-light test test:(test text-bold)'
-    const correctClasses =
+    const correct =
       'bg-white font-light sm:hover:(bg-gray-100 font-medium) test sm:hover:(test) test:(test text-bold)'
 
     const sortedClassList = windiSorter.sortClassNames(classes)
 
-    expect(sortedClassList).toBe(correctClasses)
+    expect(sortedClassList).toBe(correct)
   })
 })
 
 describe('Has Important Sort', () => {
-  const windiSorter = new WindiSorter()
+  it('Without group syntax', () => {
+    const windiSorter = new WindiSorter({ useVariantGroup: false })
 
-  it('TailwindCSS syntax', () => {
     const classes =
       'z-10 container !sm:hover:font-medium text-left md:!text-center justify-center'
-    const correctClasses =
+    const correct =
       'container justify-center text-left z-10 sm:hover:!font-medium md:!text-center'
 
     const sortedClassList = windiSorter.sortClassNames(classes)
 
-    expect(sortedClassList).toBe(correctClasses)
+    expect(sortedClassList).toBe(correct)
   })
 
-  it('WindiCSS syntax & In parentheses', () => {
+  it('With group syntax & In parentheses', () => {
+    const windiSorter = new WindiSorter({ useVariantGroup: true })
+
     const classes = 'bg-white sm:hover:(!font-medium bg-gray-100) !font-light'
-    const correctClasses =
-      'bg-white !font-light sm:hover:(bg-gray-100 !font-medium)'
+    const correct = 'bg-white !font-light sm:hover:(bg-gray-100 !font-medium)'
 
     const sortedClassList = windiSorter.sortClassNames(classes)
 
-    expect(sortedClassList).toBe(correctClasses)
+    expect(sortedClassList).toBe(correct)
   })
 
-  it('WindiCSS syntax & Out parentheses', () => {
-    const classes = 'bg-white  !font-light'
-    const correctClasses =
-      'bg-white !font-light sm:hover:(!bg-gray-100 !font-medium)'
+  it('With group syntax & Out parentheses', () => {
+    const windiSorter = new WindiSorter({ useVariantGroup: true })
+
+    const classes = 'bg-white !sm:hover:(font-medium bg-gray-100) !font-light'
+    const correct = 'bg-white !font-light sm:hover:(!bg-gray-100 !font-medium)'
 
     const sortedClassList = windiSorter.sortClassNames(classes)
 
-    expect(sortedClassList).toBe(correctClasses)
+    expect(sortedClassList).toBe(correct)
   })
 })
