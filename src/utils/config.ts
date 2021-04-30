382dafc06eeb60092299d9fb7ff5d1646ae8cf6a
suzukey <suzukei0208@gmail.com>
2021-04-30 23:37:32 +0900

    ➖ Remove find-up from dependencies

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,17 +1,5 @@
-// import findUp from 'find-up'
 import { Config } from 'windicss/types/interfaces'
 
 export function resolveConfig(): Config {
   return {}
 }
-
-// function readConfig(path?: string): NodeRequire | undefined {
-//   if (path === undefined) {
-//     path = findUp.sync('tailwind.config.js', {
-//       cwd: __dirname,
-//     })
-//     if (!path) return
-//   }
-
-//   return require(path)
-// }
