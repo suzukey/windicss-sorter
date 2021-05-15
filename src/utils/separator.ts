a871d689dea9084b2119f836d6b2f0557dc21dbc
suzukey <suzukei0208@gmail.com>
2021-05-15 17:43:37 +0900

    🚧 Remove recursive function from Separator

--- src/utils/separator.ts
+++ src/utils/separator.ts
@@ -1,69 +1,65 @@
-import type { Separate, Element } from '@/interfaces'
+import type { SeparatedElements, SorterElement } from '@/interfaces'
 
 import { isSameArray } from '@/utils'
 
 export default class Separator {
-  private sorterElements: Element[]
-  private windiElements: Element[]
-  private unknownElements: Element[]
+  private sorterElements: SorterElement[]
   private unknownClassList: Array<string>
 
-  constructor(sorterElements: Element[], unknownClassList: Array<string>) {
-    this.windiElements = []
-    this.unknownElements = []
-
+  constructor(
+    sorterElements: SorterElement[],
+    unknownClassList: Array<string>
+  ) {
     this.sorterElements = sorterElements
     this.unknownClassList = unknownClassList
   }
 
-  // TODO: Refactor method
-  public separate(): Separate {
-    this.sorterElements.forEach((el) => this._separate(el, []))
+  public separate(): SeparatedElements {
+    const windiElements: SorterElement[] = []
+    const unknownElements: SorterElement[] = []
+
+    this.sorterElements.forEach((el) => {
+      // Element with variants
+      if (Array.isArray(el.content)) {
+        el.content.forEach((innerEl) => {
+          const target = this.isWindiUtility(innerEl.content, el.variants)
+            ? windiElements
+            : unknownElements
+
+          const groupEl = target.find((targetEl) =>
+            isSameArray(targetEl.variants, el.variants)
+          )
+          // Already exist in target array
+          if (groupEl && Array.isArray(groupEl.content)) {
+            groupEl.content.push(innerEl)
+          } else {
+            target.push({
+              content: [innerEl],
+              variants: el.variants,
+              important: false,
+            })
+          }
+        })
+      }
+      // Element without variants
+      else if (typeof el.content === 'string') {
+        const target = this.isWindiUtility(el.content)
+          ? windiElements
+          : unknownElements
+        target.push(el)
+      }
+    })
 
     return {
-      windiElements: this.windiElements,
-      unknownElements: this.unknownElements,
+      windiElements,
+      unknownElements,
     }
   }
 
-  private _separate(el: Element, vars: Array<string>) {
-    const variants = vars.concat(el.variants)
-
-    // If Element type Group
-    if (el.variants.length && Array.isArray(el.content)) {
-      el.content.forEach((el) => this._separate(el, variants))
-    }
-    // Element with variants
-    else if (variants.length && typeof el.content === 'string') {
-      const e: Element = {
-        content: el.content,
-        variants: [],
-        important: el.important,
-      }
-
-      const target = this.isWindiUtility(el.content, variants)
-        ? this.windiElements
-        : this.unknownElements
-
-      const groupEl = target.find((resEl) =>
-        isSameArray(resEl.variants, variants)
-      )
-      if (groupEl && Array.isArray(groupEl.content)) {
-        groupEl.content.push(e)
-      } else {
-        target.push({ content: [e], variants, important: false })
-      }
-    }
-    // Element without variants
-    else if (typeof el.content === 'string') {
-      const target = this.isWindiUtility(el.content, variants)
-        ? this.windiElements
-        : this.unknownElements
-      target.push(el)
-    }
-  }
-
-  private isWindiUtility(className: string, variants: Array<string>): boolean {
+  private isWindiUtility(
+    className: string,
+    variants: Array<string> = []
+  ): boolean {
     const isUnknown = this.unknownClassList.find((cls) => {
       const clsHeads = cls.split(':')
       const clsName = clsHeads.pop()
